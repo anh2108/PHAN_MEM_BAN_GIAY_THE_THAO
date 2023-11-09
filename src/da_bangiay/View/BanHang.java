61d2d9617326258d4792a98be6e5218d8ee9977f
anh2108 <118011163+anh2108@users.noreply.github.com>
2023-11-09 10:34:45 +0700

    update bán hàng

--- src/da_bangiay/View/BanHang.java
+++ src/da_bangiay/View/BanHang.java
@@ -86,6 +86,7 @@ public class BanHang extends javax.swing.JFrame {
         setBackground(new java.awt.Color(255, 255, 255));
 
         jPanel1.setBackground(new java.awt.Color(204, 255, 255));
+        jPanel1.setEnabled(false);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
