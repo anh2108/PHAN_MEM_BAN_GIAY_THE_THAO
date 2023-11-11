07a739f962afecc4274a698f18279de9c8625af9
ThuTrang <149512813+thutrang2904@users.noreply.github.com>
2023-11-12 03:15:57 +0700

    ChucVu

--- src/da_bangiay/Model/ChucVu.java
+++ src/da_bangiay/Model/ChucVu.java
@@ -1,13 +1,85 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package da_bangiay.Model;
 
-/**
- *
- * @author thutr
- */
+import java.util.Date;
+
+
 public class ChucVu {
+    private int id;
+    private String hoTen;
+    private int nguoiTao;
+    private int nguoiSua;
+    private Date ngayTao;
+    private Date ngaySua;
+
+    public ChucVu() {
+    }
+
+    public ChucVu(int id, String hoTen, int nguoiTao, int nguoiSua, Date ngayTao, Date ngaySua) {
+        this.id = id;
+        this.hoTen = hoTen;
+        this.nguoiTao = nguoiTao;
+        this.nguoiSua = nguoiSua;
+        this.ngayTao = ngayTao;
+        this.ngaySua = ngaySua;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getNguoiTao() {
+        return nguoiTao;
+    }
+
+    public void setNguoiTao(int nguoiTao) {
+        this.nguoiTao = nguoiTao;
+    }
+
+    public int getNguoiSua() {
+        return nguoiSua;
+    }
+
+    public void setNguoiSua(int nguoiSua) {
+        this.nguoiSua = nguoiSua;
+    }
+
+    public Date getNgayTao() {
+        return ngayTao;
+    }
+
+    public void setNgayTao(Date ngayTao) {
+        this.ngayTao = ngayTao;
+    }
+
+    public Date getNgaySua() {
+        return ngaySua;
+    }
+
+    public void setNgaySua(Date ngaySua) {
+        this.ngaySua = ngaySua;
+    }
     
+    public Object[] toDataRow(){
+        return new Object[]{
+            this.id,
+            this.hoTen,
+            this.nguoiTao,
+            this.nguoiSua,
+            this.ngayTao,
+            this.ngaySua
+        };
+    }
 }
