bf9da9f49c8f2ce1524603455c213935ae847f56
Hoai Thu <149553436+thunh104@users.noreply.github.com>
2023-11-12 02:15:28 +0700

    Class KhachHang

--- src/da_bangiay/Model/KhachHang.java
+++ src/da_bangiay/Model/KhachHang.java
@@ -1,4 +1,103 @@
 package da_bangiay.Model;
+
+import java.util.Date;
+
 public class KhachHang {
-    
+
+    private int id;
+    private String name;
+    private long phone;
+    private String gender;
+    private String email;
+    private int createBy;
+    private int updateBy;
+    private Date createAt;
+    private Date updateAt;
+
+    public KhachHang() {
+    }
+
+    public KhachHang(int id, String name, long phone, String gender, String email, int createBy, int updateBy, Date createAt, Date updateAt) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.gender = gender;
+        this.email = email;
+        this.createBy = createBy;
+        this.updateBy = updateBy;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(int createBy) {
+        this.createBy = createBy;
+    }
+
+    public int getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(int updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
 }
