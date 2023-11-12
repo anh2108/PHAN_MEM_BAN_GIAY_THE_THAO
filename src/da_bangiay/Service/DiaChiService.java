adcc0689022924ada888cca8c603e0f929a71096
Hoai Thu <149553436+thunh104@users.noreply.github.com>
2023-11-12 21:43:48 +0700

    Update DiaChiService

--- src/da_bangiay/Service/DiaChiService.java
+++ src/da_bangiay/Service/DiaChiService.java
@@ -14,14 +14,14 @@ public class DiaChiService {
     Connection con = null;
 
     public List<DiaChi> getAll() {
-        sql = "SELECT id, id_KhachHang, Duong, ThanhPho, Tinh, QuocGia, Status, Create_by, LanSuaCuoi, Create_at, Deleted FROM tbl_DiaChi";
+        sql = "SELECT id, Duong, Tinh, ThanhPho, NguoiTao, NguoiSua, NgayTao, NgaySua FROM DiaChi";
         List<DiaChi> listdc = new ArrayList<>();
         try {
             con = DBConnect.getConnection();
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
             while (rs.next()) {
-                DiaChi dc = new DiaChi(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getInt(8), rs.getInt(9), rs.getDate(10), rs.getInt(11));
+                DiaChi dc = new DiaChi(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getDate(8), rs.getDate(9));
                 listdc.add(dc);
             }
             return listdc;
@@ -32,7 +32,7 @@ public class DiaChiService {
     }
 
     public DiaChi getDC(int id) {
-        sql = "SELECT tbl_DiaChi.id, id_KhachHang, Duong, ThanhPho, Tinh, QuocGia, Status, tbl_DiaChi.Create_by, LanSuaCuoi, tbl_DiaChi.Create_at, Deleted FROM tbl_DiaChi JOIN tbl_KhachHang ON tbl_KhachHang.id = tbl_DiaChi.id_KhachHang WHERE tbl_DiaChi.id = ?";
+        sql = "SELECT DiaChi.id, Duong, Tinh, ThanhPho, DiaChi.NguoiTao, DiaChi.NguoiSua, DiaChi.NgayTao, DiaChi.NgaySua FROM DiaChi JOIN KhachHang ON KhachHang.id_DiaChi = DiaChi.id WHERE DiaChi.id = ?";
         DiaChi dc = null;
         try {
             con = DBConnect.getConnection();
@@ -40,7 +40,7 @@ public class DiaChiService {
             ps.setObject(1, id);
             rs = ps.executeQuery();
             while (rs.next()) {
-                dc = new DiaChi(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getInt(8), rs.getInt(9), rs.getDate(10), rs.getInt(11));
+                dc = new DiaChi(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getInt(6), rs.getInt(7), rs.getDate(8), rs.getDate(9));
             }
             return dc;
         } catch (Exception e) {
@@ -50,23 +50,17 @@ public class DiaChiService {
     }
 
     public int insert(DiaChi dc) {
-        sql = "INSERT INTO dbo.tbl_DiaChi(id, id_KhachHang, Duong, ThanhPho, Tinh, QuocGia, Status, Create_by, LanSuaCuoi, Create_at, Deleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        sql = "INSERT INTO DiaChi(Duong, Tinh, ThanhPho, NguoiTao, NguoiSua, NgayTao, NgaySua) VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             con = DBConnect.getConnection();
             ps = con.prepareStatement(sql);
-            ps.setObject(1, dc.getId());
-            ps.setObject(2, dc.getIdKH());
-            ps.setObject(3, dc.getDuong());
-            ps.setObject(4, dc.getThanhPho());
-            ps.setObject(5, dc.getTinh());
-            ps.setObject(6, dc.getQuocGia());
-            ps.setObject(7, dc.getTrangThai());
-            ps.setObject(8, dc.getCreateBy());
-            ps.setObject(9, dc.getLanSC());
-            ps.setObject(10, dc.getCreateAt());
-            ps.setObject(11, dc.getDeleted());
-            ps.setObject(9, dc.getCreateAt());
-            ps.setObject(9, dc.getDeleted());
+            ps.setObject(1, dc.getDuong());
+            ps.setObject(2, dc.getTinh());
+            ps.setObject(3, dc.getThanhPho());
+            ps.setObject(4, dc.getCreateBy());
+            ps.setObject(5, dc.getUpdateBy());
+            ps.setObject(6, dc.getCreateAt());
+            ps.setObject(7, dc.getUpdateAt());
             return ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
@@ -75,19 +69,17 @@ public class DiaChiService {
     }
 
     public int update(DiaChi dc, int id) {
-        sql = "UPDATE tbl_DiaChi SET Duong = ?, ThanhPho = ?, Tinh = ?, QuocGia = ?, Status = ?, Create_by = ?, LanSuaCuoi = ?, Create_at = ?, Deleted = ? WHERE id = ?";
+        sql = "UPDATE DiaChi SET Duong = ?, Tinh = ?, ThanhPho = ?, NguoiTao = ?, NguoiSua = ?, NgayTao = ?, NgaySua = ? WHERE id = ?";
         try {
             con = DBConnect.getConnection();
             ps.setObject(1, dc.getDuong());
-            ps.setObject(2, dc.getThanhPho());
-            ps.setObject(3, dc.getTinh());
-            ps.setObject(4, dc.getQuocGia());
-            ps.setObject(5, dc.getTrangThai());
-            ps.setObject(6, dc.getCreateBy());
-            ps.setObject(7, dc.getLanSC());
-            ps.setObject(8, dc.getCreateAt());
-            ps.setObject(9, dc.getDeleted());
-            ps.setObject(10, id);
+            ps.setObject(2, dc.getTinh());
+            ps.setObject(3, dc.getThanhPho());
+            ps.setObject(4, dc.getCreateBy());
+            ps.setObject(5, dc.getUpdateBy());
+            ps.setObject(6, dc.getCreateAt());
+            ps.setObject(7, dc.getUpdateAt());
+            ps.setObject(8, id);
             return ps.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
@@ -96,7 +88,7 @@ public class DiaChiService {
     }
 
     public int delete(int id) {
-        sql = "DELETE FROM tbl_DiaChi WHERE id = ?";
+        sql = "DELETE FROM DiaChi WHERE id = ?";
         try {
             con = DBConnect.getConnection();
             ps = con.prepareStatement(sql);
